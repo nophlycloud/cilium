58dd90665bae8f30d187b524d3871944264a3032
Phaedra <72594885+nophlyzone@users.noreply.github.com>
2024-07-31 10:24:14 -0400

    Update custom_secrets_test.go

--- custom_secrets_test.go
+++ custom_secrets_test.go
@@ -1,5 +1,1 @@
-// ruleid: notion_hard_coded_bot_token
-const privateKey = 'secret_7UVlpuBIQnjPKxUynJ7eiSUO4lTs3gZtiLGOGxQECbs'
-
-// ok: notion_hard_coded_bot_token
-const f = 'secret_notreal'
+noooo
